d435d9f4d51b27c3980b1ccccd6dfa6b9bea981e
unknown <kiarie7mbugua@gmail.com>
2023-07-28 11:03:09 +0300

    -0-||-1-

--- src/main.jsx
+++ src/main.jsx
@@ -1,49 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createBrowserRouter, Route, RouterProvider } from 'react-router-dom';
-import About from './pages/About.jsx';
-import Contact from './pages/contact.jsx';
-import Help from './pages/help.jsx';
-import Worklist from './joblist/WorkList.jsx';
-import Job from './components/job/job.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, Route, RouterProvider } from "react-router-dom";
+import About from "./pages/About.jsx";
+import Contact from "./pages/contact.jsx";
+import Help from "./pages/help.jsx";
+import Worklist from "./joblist/WorkList.jsx";
+import Job from "./components/job/Job.jsx";
 //theme provider
-import { ThemeProvider } from './context/themecontext.jsx';
-
+import { ThemeProvider } from "./context/themecontext.jsx";
 
 const path = createBrowserRouter([
   {
-    path:'/',
-    element:<App />
+    path: "/",
+    element: <App />,
   },
   {
-    path:'/about',
-    element:<About />
+    path: "/about",
+    element: <About />,
   },
   {
-    path:'/contact',
-    element:<Contact />
+    path: "/contact",
+    element: <Contact />,
   },
   {
-    path:'/help',
-    element:<Help />
+    path: "/help",
+    element: <Help />,
   },
   {
-    path:'/worklist',
-    element:<Worklist/>
+    path: "/worklist",
+    element: <Worklist />,
   },
   {
-    path:'/worklist/:id',
-    element:<Job/>
-  }
-  
-])
+    path: "/worklist/:id",
+    element: <Job />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-    <RouterProvider router={path} />
+      <RouterProvider router={path} />
     </ThemeProvider>
   </React.StrictMode>
-)
+);
